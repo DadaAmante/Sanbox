e2c7df515a2e64147a496e5a4b38f20eafb19ad5
DadaAmante <133227327+DadaAmante@users.noreply.github.com>
2023-05-17 21:20:04 +0800

    added Tasklist project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {id: 1, name: "Name1", completed: true},
+    {id: 2, name: "Name2", completed: false},
+    {id: 3, name: "Name3", completed: true},
+    {id: 4, name: "Name4", completed: true},
+    {id: 5, name: "Name5", completed: true}
+  ]);
+
+  const [show, setShow] = useState(false);
+  
+  function handleDelete(id) {
+    setTasks(tasks.filter(task => id !== task.id));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+        <h1>Task list</h1>
+        <ul>
+          <button className={show ? "hide" : "show"} onClick={() => setShow(!show)}>Show</button>
+          <button className={show ? "show" : "hide"} onClick={() => setShow(!show)}>Hide</button>
+          { show && tasks.map((task) => (
+            <li key={task.id} className={task.completed ? "completed" : "incomplete"} >
+              <span>{task.id} - {task.name}</span>
+              <button onClick={() => handleDelete(task.id)}>Delete</button>
+            </li>
+          )) }
+        </ul>
+      </>
     </div>
   );
 }
